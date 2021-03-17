0ec6b7ebb9e8cf811ba2a0fc81235d9548427160
Rafael Bautista <rafael@alice.design>
2021-03-17 23:18:24 +0800

    - create checkout - create cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Login } from './pages/login';
 import { SignUp } from './pages/signup';
+import { Cart } from './pages/Cart';
+import { Checkout } from './pages/Checkout';
 
 import './App.scss';
 import './assets/scss/theme.scss';
@@ -12,6 +14,8 @@ function App() {
       <BrowserRouter>
         <Route exact path='/' component={Login} />
         <Route exact path='/register' component={SignUp} />
+        <Route exact path='/cart' component={Cart} />
+        <Route exact path='/checkout' component={Checkout} />
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,171 @@
+import React from 'react';
+import { Header } from '../components/partials/header';
+import { Footer } from '../components/partials/footer';
+import Input from "../components/shared/input";
+
+export const Checkout = () => {
+    return (
+        <>
+            <div className="cart-page">
+                <div className="container-fluid">
+                    <div className="row row-wrapper">
+                        <div chlassName="col-12">
+                            <h1 className="section-title">Place Order</h1>
+                        </div>
+                    </div>
+                    <div className="row row-wrapper">
+                        <div className="col-md-8">
+                            <div className="card product-card position-relative">
+                                <div className="row ">
+                                    <div className="position-absolute" style={{ right: "0" }}>
+                                        <a>
+                                            <img className="delete-icon mr-4 mt-4" src={require("../assets/img/delete_icon.svg")} />
+                                        </a>
+                                    </div>
+                                    <div className="col-md-5 product-wrapper">
+                                        <img className="product-image" src={require("../assets/img/product-placeholder.svg")} />
+                                    </div>
+                                    <div className="col-md-6 offset-md-1 product-desc-wrapper">
+                                        <p className="product-name mb-0">
+                                            Colesevelam 625mg Tabs 180ct
+                                        </p>
+                                        <p className="variant-text mb-0">
+                                            Brand: Welchol
+                                        </p>
+                                        <p className="price-text mb-0">
+                                            $ 999.99
+                                        </p>
+                                        <p className="quantity-text">x1</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card product-card position-relative">
+                                <div className="row ">
+                                    <div className="position-absolute" style={{ right: "0" }}>
+                                        <a>
+                                            <img className="delete-icon mr-4 mt-4" src={require("../assets/img/delete_icon.svg")} />
+                                        </a>
+                                    </div>
+                                    <div className="col-md-5 product-wrapper">
+                                        <img className="product-image" src={require("../assets/img/product-placeholder.svg")} />
+                                    </div>
+                                    <div className="col-md-6 offset-md-1 product-desc-wrapper">
+                                        <p className="product-name mb-0">
+                                            Colesevelam 625mg Tabs 180ct
+                                        </p>
+                                        <p className="variant-text mb-0">
+                                            Brand: Welchol
+                                        </p>
+                                        <p className="price-text mb-0">
+                                            $ 999.99
+                                        </p>
+                                        <p className="quantity-text">x1</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card product-card position-relative">
+                                <div className="row ">
+                                    <div className="position-absolute" style={{ right: "0" }}>
+                                        <a>
+                                            <img className="delete-icon mr-4 mt-4" src={require("../assets/img/delete_icon.svg")} />
+                                        </a>
+                                    </div>
+                                    <div className="col-md-5 product-wrapper">
+                                        <img className="product-image" src={require("../assets/img/product-placeholder.svg")} />
+                                    </div>
+                                    <div className="col-md-6 offset-md-1 product-desc-wrapper">
+                                        <p className="product-name mb-0">
+                                            Colesevelam 625mg Tabs 180ct
+                                        </p>
+                                        <p className="variant-text mb-0">
+                                            Brand: Welchol
+                                        </p>
+                                        <p className="price-text mb-0">
+                                            $ 999.99
+                                        </p>
+                                        <p className="quantity-text">x1</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card order-card position-relative">
+                                <div className="shipping-details-wrapper">
+                                    <h1 className="order-title">Shipping and Billing</h1>
+                                    <div className="row align-items-start" style={{ flex: "0" }}>
+                                        <div className="col-md-12">
+                                            <div className="d-flex align-items-center">
+                                                <div>
+                                                    <span className="mr-3"><img src={require("../assets/img/mdi_account.svg")} /></span>
+                                                </div>
+                                                <div>
+                                                    <p className="mb-0" style={{ maxWidth: "210px" }}>John Doe</p>
+                                                </div>
+                                            </div>
+                                            <div className="d-flex align-items-start">
+                                                <div>
+                                                    <span className="mr-3"><img src={require("../assets/img/mdi_map-marker.svg")} /></span>
+                                                </div>
+                                                <div>
+                                                    <p className="mb-0" style={{ maxWidth: "210px" }}>74 Williams St. Laurel, MD 20707</p>
+                                                </div>
+                                            </div>
+                                            <div className="d-flex align-items-start">
+                                                <div>
+                                                    <span className="mr-3"><img src={require("../assets/img/mdi_phone.svg")} /></span>
+                                                </div>
+                                                <div>
+                                                    <p className="mb-0">123456789</p>
+                                                </div>
+                                            </div>
+                                            <div className="d-flex align-items-start">
+                                                <div>
+                                                    <span className="mr-3"><img src={require("../assets/img/mdi_email.svg")} /></span>
+                                                </div>
+                                                <div>
+                                                    <p className="mb-0" >johndoe@gmail.com</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="order-summary-wrapper">
+                                    <h1 className="order-title">Order Summary</h1>
+                                    <div className="row align-items-start" style={{ flex: "0" }}>
+                                        <div className="col-md-8">
+                                            <p className="subtotal-text">Subtotal (3 items)</p>
+                                        </div>
+                                        <div className="col-md-4 text-right">
+                                            <p className="mb-0 price-text">$300.00</p>
+                                        </div>
+                                    </div>
+                                    <div className="row mb-5" >
+                                        <div className="col-md-8">
+                                            <p className="mb-0 shipping-text">Shipping Fee</p>
+                                            <p className="mb-0 shipping-desc">Add $xx for free shipping</p>
+                                        </div>
+                                        <div className="col-md-4 text-right">
+                                            <p className="mb-0 price-text">$20.00</p>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-7">
+                                            <p className="total-text">Total</p>
+                                        </div>
+                                        <div className="col-md-5 text-right">
+                                            <p className="mb-0 total-price-text">$320.00</p>
+                                        </div>
+                                    </div>
+                                    <div className="row justify-content-end" style={{ flex: "0" }}>
+                                        <button type="submit" className="btn button-wrapper">Proceed to Checkout</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+};

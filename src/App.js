b78db4d17fd27c7426d573bb0869ac7a95224306
Rafael Bautista <rafael@alice.design>
2021-03-19 21:52:12 +0800

    - update router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Login } from './pages/Login';
-import { SignUp } from './pages/Signup';
-import { Cart } from './pages/Cart';
-import { Checkout } from './pages/Checkout';
-import { Payment } from './pages/Payment';
-import { PaymentConfirmation } from './pages/PaymentConfirmation';
-import { ErrorPage } from './pages/error/404page';
-import './App.scss';
-import './assets/scss/theme.scss';
-import 'antd/dist/antd.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/Signup";
+import { Cart } from "./pages/Cart";
+import { Checkout } from "./pages/Checkout";
+import { Payment } from "./pages/Payment";
+import { PaymentConfirmation } from "./pages/PaymentConfirmation";
+import { ErrorPage } from "./pages/error/404page";
+import "./App.scss";
+import "./assets/scss/theme.scss";
+import "antd/dist/antd.css";
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Route exact path='/' component={Login} />
-        <Route exact path='/register' component={SignUp} />
-        <Route exact path='/cart' component={Cart} />
-        <Route exact path='/checkout' component={Checkout} />
-        <Route exact path='/payment' component={Payment} />
-        <Route exact path='/payment-confirmation' component={PaymentConfirmation} />
-        <Route path='*' exact={true} component={ErrorPage} />
-      </BrowserRouter>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={SignUp} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/payment" component={Payment} />
+          <Route
+            exact
+            path="/payment-confirmation"
+            component={PaymentConfirmation}
+          />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }

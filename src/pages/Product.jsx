80630fd34cb4f0997c2ad52c65885c15729a9927
Rafael Bautista <rafael@alice.design>
2021-03-26 01:57:18 +0800

    - update cart and product responsive

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { HeaderNav } from '../components/partials/HeaderNav';
 import { Footer } from '../components/partials/Footer';
 import ImageProduct from '../assets/img/product-sample.png';
@@ -8,42 +8,58 @@ export default props => {
     return (
         <>
             <HeaderNav />
-                <div className="product-page">
-                    <div className="container-fluid content">
-                        <div className="row">
-                            <div className="col">
-                                <div className="card medsurg-product">
-                                    <div className="img-container">
-                                        <img src={ImageProduct} alt="" />
-                                    </div>
-                                    <p>Description: </p>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh orci sit neque risus sed semper accumsan elit. 
-                                    </p>
-                                    <ul>
-                                        <li>Item #:  Lorem Ipsum</li>
-                                        <li>Manufacturer: Lorem Ipsum</li>
-                                        <li>Size: Lorem Ipsum</li>
-                                        <li>Strength: Lorem Ipsum</li>
-                                        <li>PPU: Lorem Ipsum</li>
-                                    </ul>
+            <div className="product-page">
+                <div className="container-fluid content">
+                    <div className="row">
+                        <div className="col">
+                            <div className="card medsurg-product">
+                                <div className="img-container">
+                                    <img src={ImageProduct} alt="" />
                                 </div>
-                            </div>
-                            <div className="col">
-                                <div className="card">
+                                {/* Start Mobile */}
+                                <div className="d-block d-sm-none">
                                     <h3 className="name">Colesevelam 625mg Tabs 180ct Brand: Welchol</h3>
                                     <h2 className="price">$ 999.99</h2>
+                                </div>
+                                {/* End Mobile */}
+                                <p>Description: </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh orci sit neque risus sed semper accumsan elit.
+                                    </p>
+                                <ul>
+                                    <li>Item #:  Lorem Ipsum</li>
+                                    <li>Manufacturer: Lorem Ipsum</li>
+                                    <li>Size: Lorem Ipsum</li>
+                                    <li>Strength: Lorem Ipsum</li>
+                                    <li>PPU: Lorem Ipsum</li>
+                                </ul>
+                                {/* Start Mobile */}
+                                <div className="d-block d-sm-none">
                                     <div className="d-flex align-items-center justify-container-center qty-container">
-                                        <button className="minus-btn" onClick={()=> quantity === 1 ? null : setQuantity(quantity - 1)}>-</button>
+                                        <button className="minus-btn" onClick={() => quantity === 1 ? null : setQuantity(quantity - 1)}>-</button>
                                         <span>{quantity}</span>
-                                        <button className="plus-btn" onClick={()=> setQuantity(quantity + 1)}>+</button>
+                                        <button className="plus-btn" onClick={() => setQuantity(quantity + 1)}>+</button>
                                     </div>
                                     <button className="cart-btn">Add to cart</button>
                                 </div>
+                                {/* End Mobile */}
+                            </div>
+                        </div>
+                        <div className="col d-none d-sm-block">
+                            <div className="card">
+                                <h3 className="name">Colesevelam 625mg Tabs 180ct Brand: Welchol</h3>
+                                <h2 className="price">$ 999.99</h2>
+                                <div className="d-flex align-items-center justify-container-center qty-container">
+                                    <button className="minus-btn" onClick={() => quantity === 1 ? null : setQuantity(quantity - 1)}>-</button>
+                                    <span>{quantity}</span>
+                                    <button className="plus-btn" onClick={() => setQuantity(quantity + 1)}>+</button>
+                                </div>
+                                <button className="cart-btn">Add to cart</button>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
             <Footer />
         </>
     )

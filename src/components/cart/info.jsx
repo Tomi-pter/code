431c2eabfd6be8bfbf640fe5ecdfbfc2cd3613e6
Ralph Pritos <ralphpritos@gmail.com>
2021-05-12 19:48:27 +0800

    updates

--- src/components/cart/info.jsx
+++ src/components/cart/info.jsx
@@ -3,6 +3,7 @@ import React, {useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { getAccount } from '../../actions/account';
+import Edit from '../../assets/icon/pencil.svg';
 
 export default props => {
     const dispatch = useDispatch();
@@ -14,27 +15,145 @@ export default props => {
     },[dispatch]);
 
     return (
-        <div className="customer-info">
-            <h1 className="title">Shipping and Billing</h1>
-            <ul className="info-list">
-                <li>
-                    <img src={require("../../assets/img/mdi_account.svg")} alt="" />
-                    <span className="name">{account.accountData?.given_name + ' ' + account.accountData?.family_name}</span>
-                    <span className="default">Default</span>
-                </li>
-                <li>
-                    <img src={require("../../assets/img/mdi_map-marker.svg")} alt="" />
-                    <span>{account.accountData?.address}</span>
-                </li>
-                <li>
-                    <img src={require("../../assets/img/mdi_phone.svg")} alt="" />
-                    <span>{account.accountData?.phone_number}</span>
-                </li>
-                <li>
-                    <img src={require("../../assets/img/mdi_email.svg")} alt="" />
-                    <span>{account.accountData?.email}</span>
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className="customer-info">
+                <div className="d-flex align-items-center justify-content-between title-container">
+                    <h1 className="title m-0">Shipping Address</h1>
+                    <div className="toggle">
+                        <button className="main-edit-btn" type="button" data-toggle="collapse" data-target="#infosCollapse" aria-expanded="false" aria-controls="infosCollapse">
+                            <img src={Edit} alt="" />
+                        </button>
+                        <div className="toggle-menu collapse" id="infosCollapse">
+                            <ul className="infos-list">
+                                <li>
+                                    <div className="indicator active">
+                                        <div className="center"></div>
+                                    </div>
+                                    <div className="d-flex align-items-center info-container">
+                                        <div className="info">
+                                            <div className="d-flex align-items-center name">John Doe <div className="default">Default</div></div>
+                                            <p>74 Williams St., Laurel, MD 20707</p>
+                                            <p>123456789</p>
+                                            <p>johndoe@gmail.com</p>
+                                        </div>
+                                        <button className="edit-btn">Edit</button>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="indicator">
+                                        <div className="center"></div>
+                                    </div>
+                                    <div className="d-flex align-items-center info-container">
+                                        <div className="info">
+                                            <div className="d-flex align-items-center name">John Doe</div>
+                                            <p>74 Williams St., Laurel, MD 20707</p>
+                                            <p>123456789</p>
+                                            <p>johndoe@gmail.com</p>
+                                            <button className="default-btn">Make Default</button>
+                                        </div>
+                                        <button className="edit-btn">Edit</button>
+                                    </div>
+                                </li>
+                            </ul>
+                            <button className="add-btn">+ Add New Address</button>
+                            <div className="d-flex align-items-center justify-content-between">
+                                <button className="cancel-btn">Cancel</button>
+                                <button className="save-btn">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul className="info-list">
+                    <li>
+                        <img src={require("../../assets/img/mdi_account.svg")} alt="" />
+                        <span className="name">{account.accountData?.given_name + ' ' + account.accountData?.family_name}</span>
+                        <span className="default">Default</span>
+                    </li>
+                    <li>
+                        <img src={require("../../assets/img/mdi_map-marker.svg")} alt="" />
+                        <span>{account.accountData?.address}</span>
+                    </li>
+                    <li>
+                        <img src={require("../../assets/img/mdi_phone.svg")} alt="" />
+                        <span>{account.accountData?.phone_number}</span>
+                    </li>
+                    <li>
+                        <img src={require("../../assets/img/mdi_email.svg")} alt="" />
+                        <span>{account.accountData?.email}</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="customer-info">
+                <div className="d-flex align-items-center justify-content-between title-container m-0">
+                    <h1 className="title m-0">Billing Address</h1>
+                    <div className="toggle">
+                        <button className="main-edit-btn" type="button" data-toggle="collapse" data-target="#infosCollapse2" aria-expanded="false" aria-controls="infosCollapse2">
+                            <img src={Edit} alt="" />
+                        </button>
+                        <div className="toggle-menu collapse" id="infosCollapse2">
+                            <ul className="infos-list">
+                                <li>
+                                    <div className="indicator active">
+                                        <div className="center"></div>
+                                    </div>
+                                    <div className="d-flex align-items-center info-container">
+                                        <div className="info">
+                                            <div className="d-flex align-items-center name">John Doe <div className="default">Default</div></div>
+                                            <p>74 Williams St., Laurel, MD 20707</p>
+                                            <p>123456789</p>
+                                            <p>johndoe@gmail.com</p>
+                                        </div>
+                                        <button className="edit-btn">Edit</button>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="indicator">
+                                        <div className="center"></div>
+                                    </div>
+                                    <div className="d-flex align-items-center info-container">
+                                        <div className="info">
+                                            <div className="d-flex align-items-center name">John Doe</div>
+                                            <p>74 Williams St., Laurel, MD 20707</p>
+                                            <p>123456789</p>
+                                            <p>johndoe@gmail.com</p>
+                                            <button className="default-btn">Make Default</button>
+                                        </div>
+                                        <button className="edit-btn">Edit</button>
+                                    </div>
+                                </li>
+                            </ul>
+                            <button className="add-btn">+ Add New Address</button>
+                            <div className="d-flex align-items-center justify-content-between">
+                                <button className="cancel-btn">Cancel</button>
+                                <button className="save-btn">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="checkbox-container">
+                    <input type="checkbox" id="sameAddress" name="sameAddress" value="true" />
+                    <label for="sameAddress">Same as shipping address</label>
+                </div>
+                <ul className="info-list">
+                    <li>
+                        <img src={require("../../assets/img/mdi_account.svg")} alt="" />
+                        <span className="name">{account.accountData?.given_name + ' ' + account.accountData?.family_name}</span>
+                        <span className="default">Default</span>
+                    </li>
+                    <li>
+                        <img src={require("../../assets/img/mdi_map-marker.svg")} alt="" />
+                        <span>{account.accountData?.address}</span>
+                    </li>
+                    <li>
+                        <img src={require("../../assets/img/mdi_phone.svg")} alt="" />
+                        <span>{account.accountData?.phone_number}</span>
+                    </li>
+                    <li>
+                        <img src={require("../../assets/img/mdi_email.svg")} alt="" />
+                        <span>{account.accountData?.email}</span>
+                    </li>
+                </ul>
+            </div>
+        </>
     )
 }
--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Logo from '../../assets/img/logo.svg';
-import PPLogo from '../../assets/img/pp-logo.svg'
+import PPLogo from '../../assets/img/pp-logo.svg';
 export const Header = () => {
   return (
     <nav className="navbar header-login header">
-      <a href="http://premierpharma.wpengine.com/">
+      <a href="https://premierpharma.wpengine.com/">
         <img className="logo" src={PPLogo} width="152.25" height="46.49" alt="" />
       </a>
     </nav>

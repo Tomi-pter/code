f99cd6bc3ee7280ad68efa256af23a500bb60531
Rafael Bautista <rafael@alice.design>
2021-06-04 01:46:40 +0800

    - update sign up page newsletter

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -23,12 +23,13 @@ export const SignUpContainer = () => {
           <div className="newsletterWrapper d-flex align-items-center justify-content-center mt-5">
             <div className="emailWrapper">
               <h2>Want to learn more about how Premier Pharmaceuticals can help?</h2>
-              <div className="d-flex align-items-stretch emailGroup">
+              <iframe src="http://go.pardot.com/l/906322/2021-05-28/2tw2q" width="100%" type="text/html" frameborder="0" allowTransparency="true"></iframe>
+              {/* <div className="d-flex align-items-stretch emailGroup">
                 <input type="text" className="form-control " />
                 <div className="input-group-append d-flex align-items-center">
                   <button className="" type="button">Sign up</button>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>

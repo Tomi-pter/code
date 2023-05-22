32cbea66c125ef16c85a28ed33acdf89defa4ce5
Nikko Brian Caraan <nikko@outliant.com>
2023-05-22 20:27:39 +0800

    added user-menu endpoint

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,22 +1,57 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getAvatar } from "../actions/account";
+import { getCart, getCount } from "../actions/cart";
 
 export default function Auth () {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const localUser = JSON.parse(localStorage.getItem("profile"));
+
+    const [user, setUser] = useState(null);
+    const avatar = useSelector((state) => state.account.avatarData);
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+    const location = useLocation();
+    
+    const itemCount = cart.cartData?.length > 0
+        ? cart.cartData
+            .map((item) => parseInt(item.quantity))
+            .reduce((prev, next) => prev + next)
+        : 0;
 
     useEffect(() => {
-        setTimeout(() => {
-            setIsAuthenticated(true);
-        }, 3000)
-    }, []);
+        if (localUser) {
+            setUser(localUser);
+            dispatch(getCart(localUser?.username));
+            dispatch(getCount(localUser?.username));
+            dispatch(getAvatar(localUser?.username));
+        }
+    }, [location])
 
     useEffect(() => {
-        if (isAuthenticated && window.location !== window.parent.location) {
+        sendWebflowData();
+    }, [user, cart, avatar])
+
+    const sendWebflowData = () => {
+        if (location.pathname === "/login" || location.pathname === "/register") {
             window.parent.postMessage(
-                { userData: { cartCount: 5, avatar: "avatar.svg" } },
-                "https://premier-pharma.webflow.io"
+                null,
+                process.env.REACT_APP_HOMEPAGE_URL
             )
         }
-    }, [isAuthenticated]);
+        else {
+            const avatarData = avatar !== "" && !Array.isArray(avatar)
+                ? avatar
+                : `https://stage.premierpharma.com/wp-content/uploads/2021/05/placeholder-dp.svg`;
 
-    return !isAuthenticated && "Loading ..."
+            const sendData = { ...user, avatarData, cartCount: itemCount };
+            window.parent.postMessage(
+                sendData,
+                process.env.REACT_APP_HOMEPAGE_URL
+            )
+        }
+    }
+
+    return <></>
 }
